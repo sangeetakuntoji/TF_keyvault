27c64f92a87eea01d3da7651ed532050e8d3e9ef
sangeetakuntoji <139916774+sangeetakuntoji@users.noreply.github.com>
2023-07-31 17:24:12 +0200

    tf version updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,10 @@
 terraform {
-  required_version = "3.66.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.66.0"
+    }
+  }
 }
 
 provider "azurerm" {
