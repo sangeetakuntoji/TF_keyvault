444449e1008e14a38ff53c44f5b2fbf28098db2b
sangeetakuntoji <139916774+sangeetakuntoji@users.noreply.github.com>
2023-07-31 17:13:40 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,16 +1,5 @@
 terraform {
-  required_version = ">=2.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  }
+  required_version = "3.66.0"  
 }
 
 provider "azurerm" {
