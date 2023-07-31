05b87f0ae740b9c3babfd5e61a7c4b965038131a
sangeetakuntoji <139916774+sangeetakuntoji@users.noreply.github.com>
2023-07-31 18:57:34 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,15 +10,7 @@ terraform {
 provider "azurerm" {
   features {}
 }
-# backend configuration
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "dev-rg"
-    storage_account_name = "devxyzstorageaccount1"
-    container_name       = "devconatiner"
-    key                  = "devkeyvault.tfstate"
-  }
-}
+
 
 
 
